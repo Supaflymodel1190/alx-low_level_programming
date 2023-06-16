87605ba80760fe553624b58331c834397e9216be
Supaflymodel1190 <enahoroa7@gmail.com>
2023-06-15 19:15:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.n>
+/**
+ * main - Entry poin
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+
+ puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
